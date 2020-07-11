50dd926553855ef504cc0c15584a5651e4833733
Vaughn Friesen <vdfriesen@vaughnfriesen.com>
2020-07-11 14:50:35 +0800

    Add autosuggest

--- zsh.sh
+++ zsh.sh
@@ -18,6 +18,9 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 # Install powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Copy config files
 cp zshrc "$HOME/.zshrc"
 cp p10k.zsh "$HOME/.p10k.zsh"
